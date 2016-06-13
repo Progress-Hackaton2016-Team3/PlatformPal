8bf08353d1aeddbeafe49afba6fde8afe6be99e5
Ivan I. Ivanov <ivani.ivanov@telerik.com>
2016-06-13 09:21:08 +0000

    Add Status Codes util

---
+++ Tasks/app/utils/statusCodes.ts
@@ -0,0 +1,13 @@
+export module StatusCodes {
+    export function isUnauthorizedError(statusCode: number): boolean {
+        return statusCode === 401;
+    }
+
+    export function isUserError(statusCode: number): boolean {
+        return ~~(statusCode / 100) === 4;
+    }
+
+    export function isOK(statusCode: number): boolean {
+        return ~~(statusCode / 100) === 2;
+    }
+}
